0b7707c2d48f40f18c86936cbdaccde40f204c8c
wasim <w.akram.1912@protonmail.com>
2023-12-10 02:37:53 +0530

    fix: change to amplifyconfiguration

--- scripts/pre-install.js
+++ scripts/pre-install.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
 
 const buildProfile = process.env.EAS_BUILD_PROFILE;
-const configProd = process.env.AWS_EXPORTS;
-const configDevelop = process.env.AWS_EXPORTS_DEVELOP;
+const configProd = process.env.AMPLIFY_EXPORTS;
+// const configDevelop = process.env.AMPLIFY_EXPORTS_DEVELOP;
 
-const config = buildProfile === "production" ? configProd : configDevelop;
+// const config = buildProfile === "production" ? configProd : configDevelop;
 
-const decoded = Buffer.from(config, "base64").toString();
+const decoded = Buffer.from(configProd, "base64").toString();
 
-fs.writeFileSync("./src/aws-exports.js", decoded);
+fs.writeFileSync("./src/amplifyconfiguration.json", decoded);
